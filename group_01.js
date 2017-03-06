27949334e2ef0fc03ecbdde7a716385cb7344d08
Lisa Schoofs <lisaschoofs@gmail.com>
2017-03-06 15:44:31 -0600

    initial commit

--- group_01.js
+++ group_01.js
@@ -6,3 +6,58 @@ var robert = ["Robert", "26835", "66000", 1];
 var mayella = ["Mayella", "89068", "35000", 2];
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
+
+function convertEmployees(array) {
+  for (var i = 0; i < array.length; i++) {
+    var newEmployeeArray = new Object(array[i]);
+  }
+}
+
+function newEmployee(employeeArray){
+  //constructor for new employee array
+  this.name = employeeArray[0];
+  this.
+  //Figures out employee bonus percentage
+  var employeeBonus = 0;
+  function bonusPercent(rating){
+    if(newEmployeeArray[4]==5){
+      employeeBonus += '.10';
+    } else if (newEmployeeArray[4]==3) {
+      employeeBonus += '.04';
+    } else if (newEmployeeArray[4]==4) {
+      employeeBonus += '.06';
+    } else {
+      employeeBonus += 0;
+    }
+
+    if(newEmployeeArray[1].length==4){
+      employeeBonus += '.05';
+    }
+
+    if(newEmployeeArray[2]>65000)
+    {
+      employeeBonus -= '.01';
+    }
+
+    if(employeeBonus > '.13')
+    {
+      employeeBonus = '.13';
+    }
+    else if (employeeBonus < 0) {
+      employeeBonus = 0;
+    }
+
+    return employeeBonus;
+  }
+
+//figures out the total bonus
+  function totalBonus(){
+    var totalBonus = parsInt(employeeBonus*newEmployeeArray[2]);
+    return totalBonus;
+  }
+
+//figures out annual compensation
+  function totalComp(){
+    var total = (totalBonus+newEmployeeArray[2]);
+  }
+}
